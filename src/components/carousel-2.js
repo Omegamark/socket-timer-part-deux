a622f9c154e2ce0b3d5ed6dd3f3a33d2e11d7d6a
Omegamark <grant.mark@gmail.com>
2017-10-30 09:51:36 -0600

    commiting working version

---
+++ src/components/carousel-2.js
@@ -0,0 +1,36 @@
+import React, { Component } from "react";
+import { Carousel } from "react-materialize";
+import { findDOMNode } from "react-dom";
+
+class CatCarousel2 extends Component {
+  interval = null;
+  ref = null;
+
+  componentDidMount = () => {
+    this.interval = window.setInterval(() => {
+      window.$(findDOMNode(this.ref)).carousel("prev"); // ".carousel-slider"
+    }, 2000);
+  };
+
+  componentWillUnmount = () => {
+    window.clearInterval(this.interval);
+  };
+
+  render = () => {
+    const { catPics } = this.props;
+
+    return (
+      <div>
+        <Carousel ref={r => (this.ref = r)} className="bottom-carousel">
+          {catPics.length ? (
+            catPics.map(image => <img key={image} src={image} />)
+          ) : (
+            <div>No pics yet</div>
+          )}
+        </Carousel>
+      </div>
+    );
+  };
+}
+
+export default CatCarousel2;
